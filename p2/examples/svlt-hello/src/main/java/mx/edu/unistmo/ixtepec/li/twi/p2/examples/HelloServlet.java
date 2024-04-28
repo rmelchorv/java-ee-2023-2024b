b84d8428f4240d80be0704d66450ad0f8fd6e25f
Ricardo E. Melchor <rmelchorv@gmail.com>
2024-04-27 23:37:43 -0600

    Hello servlet web app

---
+++ p2/examples/svlt-hello/src/main/java/mx/edu/unistmo/ixtepec/li/twi/p2/examples/HelloServlet.java
@@ -0,0 +1,57 @@
+package mx.edu.unistmo.ixtepec.li.twi.p2.examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "HelloServlet", urlPatterns = { "/HelloServlet" })
+public class HelloServlet extends HttpServlet {
+  private static final String FIRSTNAME = "firstname";
+  private static final String LASTNAME = "lastname";
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException {
+    String fname = request.getParameter(FIRSTNAME);
+    String lname = request.getParameter(LASTNAME);
+
+    if ((fname == null || fname.isBlank()) && (lname == null || lname.isBlank())) {
+      fname = getServletConfig().getInitParameter(FIRSTNAME);
+      lname = getServletConfig().getInitParameter(LASTNAME);
+    }
+    response.setCharacterEncoding("UTF-8");
+    response.setContentType("text/html");
+
+    try {
+      PrintWriter out = response.getWriter();
+      String html = String.format(
+          String.join("\n", "<!DOCTYPE html>",
+              "<html lang=\"es-mx\">",
+              " <head>",
+              "  <meta charset='UTF-8'>",
+              "  <link rel='icon' href='assets/img/favicon.png'>",
+              "  <title>%s</title>",
+              " </head>",
+              " <body>",
+              "  <h1>Hello %s %s!</h1>",
+              "  <h3>Request URI: %s</h3>",
+              "  <h3>Context path: %s</h3>",
+              "  <h3>Servlet path: %s</h3>",
+              " </body>",
+              "</html>"),
+          this.getServletName(), fname, lname,
+          request.getRequestURI(),
+          request.getContextPath(),
+          request.getServletPath());
+
+      out.println(html);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
---
+++ p2/examples/svlt-hello/src/main/webapp/index.jsp
@@ -0,0 +1,21 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<!DOCTYPE html>
+<html lang="es-mx">
+  <head>
+    <meta charset="UTF-8">
+    <title>Hello Servlet</title>
+    <link rel="icon" href="assets/img/favicon.png">
+  </head>
+  <body>
+    <h2>Hello Servlet</h2>
+    <h3>Informática, (608) – UNISTMO</h3>
+
+    <form method="get" action="HelloServlet">
+      <label for="firstname">First name:</label><br>
+      <input type="text" name="firstname" placeholder="Enter your fisrt name" autofocus><br>
+      <label for="lastname">Last name:</label><br>
+      <input type="text" name="lastname" placeholder="Enter yout last name"><br>
+      <input type="submit" value="Submit">
+    </form>
+  </body>
+</html>
